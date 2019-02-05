f03953b8193f9fdabaf4caca1fc0bed30adc87f0
Arun Jose <arunjose.81@gmail.com>
2019-02-05 22:28:02 +0000

    Added deploy files

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,13 @@
 docker build -t arunjose/multi-docker-frontend:latest -t arunjose/multi-docker-frontend:$SHA ./client/Dockerfile ./client
 docker build -t arunjose/multi-docker-backend:latest -t arunjose/multi-docker-backend:$SHA ./server/Dockerfile ./server
 docker build -t arunjose/multi-docker-worker:latest -t arunjose/multi-docker-worker:$SHA ./worker/Dockerfile ./worker
-
 docker push arunjose/multi-docker-frontend:latest
 docker push arunjose/multi-docker-backend:latest
 docker push arunjose/multi-docker-worker:latest
-
 docker push arunjose/multi-docker-frontend:$SHA
 docker push arunjose/multi-docker-backend:$SHA
 docker push arunjose/multi-docker-worker:$SHA
-
 kubectl apply -f k8s
-kubectl set image deployments/backend-deployment backend=backend-arunjose/multi-docker-backend:$GIT_SHA
-kubectl set image deployments/frontend-deployment frontend=frontend-arunjose/multi-docker-frontend:$GIT_SHA
-kubectl set image deployments/worker-deployment worker=arunjose/multi-docker-worker:$GIT_SHA
+kubectl set image deployments/backend-deployment backend=backend-arunjose/multi-docker-backend:$SHA
+kubectl set image deployments/frontend-deployment frontend=frontend-arunjose/multi-docker-frontend:$SHA
+kubectl set image deployments/worker-deployment worker=arunjose/multi-docker-worker:$SHA
